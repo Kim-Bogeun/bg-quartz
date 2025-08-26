b89f08b61a740fd04efff07d40ec08026c36e42e
Bogeun, Kim <kbg9727@gmail.com>
2025-08-26 12:49:28 +0900

    디자인 변경

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -65,7 +65,7 @@ export default ((opts?: Partial<GraphOptions>) => {
     const globalGraph = { ...defaultOptions.globalGraph, ...opts?.globalGraph }
     return (
       <div class={classNames(displayClass, "graph")}>
-        <h4>{i18n(cfg.locale).components.graph.title}</h4>
+        <h5>{i18n(cfg.locale).components.graph.title}</h5>
         <div class="graph-outer">
           <div class="graph-container" data-cfg={JSON.stringify(localGraph)}></div>
           <button class="global-graph-icon" aria-label="Global Graph">
